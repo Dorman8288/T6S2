ce6a57cb36196f459edffe82eea06c7dbffc553f
Dorman <dorman8288@gmail.com>
2022-05-18 18:03:18 +0430

    45

--- 6-2-final/source/Accounts/Account.cs
+++ 6-2-final/source/Accounts/Account.cs
@@ -7,6 +7,7 @@ namespace tamrin_6_2_final.source
     class Account
     {
         string name;
+        List<MediaClasses.Media> cart = new List<MediaClasses.Media>();
         public Account(string name)
         {
             this.name = name;
--- 6-2-final/source/DataManagement/DataManager.cs
+++ 6-2-final/source/DataManagement/DataManager.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Text.Json;
 
 namespace tamrin_6_2_final.source.DataManagement
 {
-    class DataManager
+    static class DataManager
     {
-        //public virtual save(object input);
+        static List<MediaClasses.Media> medias = new List<MediaClasses.Media>();
+        static List<Account> accounts = new List<Account>();
+        public static void SaveAll()
+        {
+            SaveAccounts();
+            SaveMedias();
+        }
+        public static void SaveAccounts()
+        {
+            if (File.Exists("Accounts.json")) File.Delete("Accounts.json");
+            foreach (var account in accounts)
+            {
+                Save("Accounts.json", account);
+            }
+        }
+        public static void SaveMedias()
+        {
+            if (File.Exists("Media.json")) File.Delete("Media.json");
+            foreach (var media in medias)
+            {
+                Save("Media.json", media);
+            }
+        }
+        public static void Save(string path, object input)
+        {
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine(JsonSerializer.Serialize(input));
+            writer.Close();
+        }
+        public static void AddAccount(Account account)
+        {
+            accounts.Add(account);
+            Save("Accounts.json", account);
+        }
+        public static void AddMedia(MediaClasses.Media media)
+        {
+            medias.Add(media);
+            Save("Accounts.json", media);
+        }
     }
 }
